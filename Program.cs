26f552b542d3b651c668fb84ba337310fed4074f
Waldo Heudens <waldo@heudens.be>
2021-10-12 11:57:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using AssynchroneVerwerking;
+
+int aantalIteraties = 10;
+DateTime starttijd = DateTime.Now;
+Console.WriteLine("Gestart op " + starttijd.ToString() + "\n");
+
+// Creëer een aantal keer een object van traagheid, en laat achtereenvolgens wachten
+for (int i = 0; i<aantalIteraties; i++)
+{
+    Traagheid traag = new Traagheid(i);
+    traag.Wacht();
+}
+
+// Check of de methode Wacht ook voor alle objecten werd uitgevoerd
+while (Traagheid.aantalKlaar < aantalIteraties)
+{
+    System.Threading.Thread.Sleep(20);
+}
+
+// Toon eindresultaten
+DateTime einde = DateTime.Now;
+TimeSpan tijdverschil = einde - starttijd;
+Console.WriteLine("\nBeëindigd op {0} na {1}s, terwijl de som van alle wachttijden {2}s is.", einde.ToString(), tijdverschil, Traagheid.somAlleWachttijden);
---
+++ Traagheid.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssynchroneVerwerking
+{
+    internal class Traagheid
+    {
+        public static int aantalKlaar = 0;  // Hoeveel asynchrone "Wachts" werden uitgevoerd?
+        public static double somAlleWachttijden = 0;   // De totale tijd in seconden besteed aan wachten 
+
+        public int Id { get; set; }
+
+        public Traagheid(int id)
+        {
+            Id = id;
+        }
+
+        public async void Wacht()
+        {
+            // Definieer een nieuwe delegate van type Func<int> die GetWachttijd uitvoerd
+            Func<int> asyncMetode = new Func<int>(GetWachttijd);
+
+            // Voer deze delegate uit in een nieuwe thread (asynchroon) en zet het resultaat in wachttijd
+            int wachttijd = await Task.Factory.StartNew(asyncMetode);
+
+            // Als de methode klaar is:  Verwerk de resultaten
+            Console.WriteLine("Klaar met {0} na {1}ms", Id, wachttijd);
+            aantalKlaar++;
+            somAlleWachttijden += wachttijd/1000;
+        }
+
+        private int GetWachttijd()
+        {
+            // wacht een random tijd tussen 0 en 2 seconden en return deze tijd
+
+            int wachttijd = new Random().Next(2000);
+            System.Threading.Thread.Sleep(wachttijd);
+            return wachttijd;
+        }
+    }
+}
